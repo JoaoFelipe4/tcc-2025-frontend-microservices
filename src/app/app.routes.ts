e0d59b73ce4fb88320b079ce0eccdd9e20b25bab
Joao Felipe <johaoflp93@gmail.com>
2025-09-24 23:25:06 -0300

    restore login and dashboard route configurations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -15,15 +15,15 @@ export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'sobre', component: SobreComponent },
- // {
- //   path: 'login',
- //   component: LoginComponent,
-//    canActivate: [GuestGuard]
-//  },
-// {
-//    path: 'dashboard',
-//    component: DashboardComponent,
-//    canActivate: [AuthGuard]
-//},
-//  { path: '**', redirectTo: '/home' }
+ {
+   path: 'login',
+   component: LoginComponent,
+   canActivate: [GuestGuard]
+ },
+{
+   path: 'dashboard',
+   component: DashboardComponent,
+   canActivate: [AuthGuard]
+},
+ { path: '**', redirectTo: '/home' }
 ];
